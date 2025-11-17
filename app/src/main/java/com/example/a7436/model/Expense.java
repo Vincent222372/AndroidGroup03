dcbe8202399ff2825ec79235a79acbb959bc0d4c
Vincent222372 <vincentbrando0@gmail.com>
2025-11-17 08:42:00 +0700

    hh

--- app/src/main/java/com/example/a7436/model/Expense.java
+++ app/src/main/java/com/example/a7436/model/Expense.java
@@ -1,8 +1,13 @@
 package com.example.a7436.model;
 
+import java.time.LocalDate;
+
 public class Expense {
     private String description;
     private double amount;
+    private LocalDate date;
+
+
 
 
 }
