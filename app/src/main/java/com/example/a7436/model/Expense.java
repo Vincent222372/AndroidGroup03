fe45f12d9d22a24340a3d4cf9fe79e3b20795781
Vincent222372 <vincentbrando0@gmail.com>
2025-11-17 08:51:56 +0700

    hh

--- app/src/main/java/com/example/a7436/model/Expense.java
+++ app/src/main/java/com/example/a7436/model/Expense.java
@@ -7,7 +7,27 @@ public class Expense {
     private double amount;
     private LocalDate date;
 
+    public String getDescription() {
+        return description;
+    }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
+    public double getAmount() {
+        return amount;
+    }
 
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
 }
