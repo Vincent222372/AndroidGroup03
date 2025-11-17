c86498e1bdbf883311b583df9b11ee88f5a87ec9
DUNCHAN <dunchan3432@gmail.com>
2025-11-17 08:52:50 +0700

    fw

--- app/src/main/java/com/example/a7436/model/Expense.java
+++ app/src/main/java/com/example/a7436/model/Expense.java
@@ -7,6 +7,12 @@ public class Expense {
     private double amount;
     private LocalDate date;
 
+    public Expense(String description, double amount, LocalDate date) {
+        this.description = description;
+        this.amount = amount;
+        this.date = date;
+    }
+
     public String getDescription() {
         return description;
     }
